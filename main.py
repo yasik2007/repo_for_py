f5547dd91dff71f919d3311fe93fd09439a06b80
yasik2007 <aleksenko.yak@gmail.com>
2026-03-25 20:41:37 +0300

    delete alg files

--- main.py
+++
@@ -1,9 +0,0 @@
-class Transaction:
-    def __init__(self,amount,currency):
-        self.amount = amount
-        self.currency = currency
-    def __str__(self):
-        return f'Транзакция на сумму {self.amount:.2f} {self.currency}'
-
-
-
